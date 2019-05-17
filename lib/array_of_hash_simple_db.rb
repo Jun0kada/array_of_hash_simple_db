f06456d0d9e56dd0fe91e2b9a9eb329d1adfcf61
Jun0kada <jun.okada0203@gmail.com>
2019-05-17 10:56:36 +0900

    Add ArrayOfHashSimpleDb

--- lib/array_of_hash_simple_db.rb
+++ lib/array_of_hash_simple_db.rb
@@ -1,5 +1,53 @@
 require 'array_of_hash_simple_db/version'
 
 module ArrayOfHashSimpleDb
-  # Your code goes here...
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    attr_writer :records
+
+    def records
+      @_initialized_records ||= (@records || []).map { |r| new(r) }
+    end
+
+    alias_method :all, :records
+
+    def find_by(condition)
+      records.detect do |record|
+        condition.all? do |k, v|
+          [v].flatten.include?(record.public_send(k))
+        end
+      end
+    end
+
+    def respond_to_missing?(name, include_private = false)
+      records.respond_to?(name) || super
+    end
+
+    def method_missing(method, *args, &block)
+      if records.respond_to?(method)
+        records.public_send(method, *args, &block)
+      else
+        super
+      end
+    end
+  end
+
+  attr_reader :attributes
+
+  def initialize(attributes)
+    @attributes = attributes
+
+    @attributes.each do |name, value|
+      self.class.send(:define_method, name) do
+        if @attributes.has_key?(name.to_s)
+          @attributes[name.to_s]
+        elsif @attributes.has_key?(name.to_sym)
+          @attributes[name.to_sym]
+        end
+      end
+    end
+  end
 end
--- spec/array_of_hash_simple_db_spec.rb
+++ spec/array_of_hash_simple_db_spec.rb
@@ -1,9 +1,99 @@
+class User
+  include ArrayOfHashSimpleDb
+
+  self.records = [
+    { name: 'Jun' },
+    { name: 'John' },
+    { name: 'Jun', age: 20 },
+  ]
+end
+
+class NotRecordsSetUser
+  include ArrayOfHashSimpleDb
+end
+
 RSpec.describe ArrayOfHashSimpleDb do
-  it 'has a version number' do
-    expect(ArrayOfHashSimpleDb::VERSION).not_to be nil
+  describe '.records=' do
+    subject { NotRecordsSetUser.instance_variable_get(:@records) }
+
+    context 'before set records' do
+      it { is_expected.to eq(nil) }
+    end
+
+    context 'after set records' do
+      let(:users) do
+        [
+          { name: 'Jun' },
+          { name: 'John' },
+        ]
+      end
+
+      before do
+        NotRecordsSetUser.records = users
+      end
+
+      it { is_expected.to eq(users) }
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.records' do
+    it 'return initialized records' do
+      expect(User.records.length).not_to eq(0)
+      expect(User.records.all? { |u| u.instance_of?(User) }).to eq(true)
+    end
+  end
+
+  describe '.find_by' do
+    context 'when exist record' do
+      let(:name) { 'Jun' }
+
+      it 'return first Jun record' do
+        user = User.find_by(name: name)
+        expect(user.name).to eq('Jun')
+        expect(user.age).to eq(nil)
+      end
+
+      context 'with multiple condition' do
+        let(:age) { 20 }
+
+        it 'return first Jun record' do
+          user = User.find_by(name: name, age: age)
+          expect(user.name).to eq('Jun')
+          expect(user.age).to eq(20)
+        end
+      end
+    end
+
+    context 'when not exist record' do
+      let(:name) { 'not exist user name' }
+
+      it 'return nil' do
+        expect(User.find_by(name: name)).to eq(nil)
+      end
+    end
+  end
+
+  describe '#attributes' do
+    subject { User.first.public_send(attribute) }
+
+    context 'when exist key' do
+      let(:attribute) { :name }
+
+      it { is_expected.to eq('Jun') }
+
+      context 'But this record has not key' do
+        let(:attribute) { :age }
+
+        it { is_expected.to eq(nil) }
+      end
+    end
+
+    context 'when not exist key' do
+      let(:attribute) { :address }
+
+      it 'raise NoMethodError' do
+        expect { subject }.to raise_error(NoMethodError)
+      end
+    end
   end
 end
